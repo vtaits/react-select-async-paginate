f96f6bcce66cf01b748b118a70e9f25cfd21267f
Vadim Taits <taizvadim@gmail.com>
2018-12-21 17:04:06 +0300

    Upgraded eslint dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest/globals": true
-    },
-    "extends": ["eslint:recommended", "airbnb"],
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "jest"
-    ],
-    "rules": {
-        "arrow-parens": ["error", "always"],
-        "react/no-did-update-set-state": "off",
-        "no-plusplus": "off",
-        "no-nested-ternary": "off",
-    }
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended', 'airbnb'],
+  parser: 'babel-eslint',
+
+  plugins: [
+    'react',
+    'jest',
+  ],
+
+  rules: {
+    'arrow-parens': ['error', 'always'],
+    'react/no-did-update-set-state': 'off',
+    'no-plusplus': 'off',
+    'no-nested-ternary': 'off',
+  }
 };
