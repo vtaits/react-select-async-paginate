e1e7f286766908f68cab3f619d3b243ed9752600
Vadim Taits <taizvadim@gmail.com>
2019-07-14 16:03:04 +0300

    Moved jest config from package.json to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  collectCoverageFrom: [
+    'packages/react-select-async-paginate/src/**/*.js',
+    'packages/react-select-async-paginate/src/**/*.jsx',
+    '!packages/react-select-async-paginate/src/**/__tests__/**/*.js',
+    '!packages/react-select-async-paginate/src/**/__tests__/**/*.jsx',
+  ],
+
+  coverageReporters: [
+    'text',
+    'lcov',
+    'html',
+  ],
+
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/examples/',
+  ],
+
+  setupFiles: [
+    './setup-jest.js',
+  ],
+};
