1ac8d9dfffa98211bc963c7adf259a10a74e23e5
echo-vladimir <echo.vladimir.k@gmail.com>
2024-08-08 18:55:59 +0200

    Restore file to its state from commit a4b2850

--- packages/react-select-async-paginate/__stories__/Simple.tsx
+++ packages/react-select-async-paginate/__stories__/Simple.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
-import type { ReactElement } from "react";
+import { useState } from 'react';
+import type {
+  ReactElement,
+} from 'react';
 
-import type { GroupBase, MultiValue } from "react-select";
+import type {
+  GroupBase,
+  MultiValue,
+} from 'react-select';
 
-import type { Meta, StoryObj } from "@storybook/react";
-import { within, userEvent, expect, waitFor, fireEvent } from "@storybook/test";
+import sleep from 'sleep-promise';
 
-import sleep from "sleep-promise";
+import { AsyncPaginate } from '../src';
+import type {
+  LoadOptions,
+} from '../src';
 
-import { AsyncPaginate } from "../src";
-import type { LoadOptions } from "../src";
-
-import type { StoryProps } from "./types";
+import type {
+  StoryProps,
+} from './types';
 
 type OptionType = {
   value: number;
@@ -27,9 +33,9 @@ for (let i = 0; i < 50; ++i) {
 }
 
 const loadOptions: LoadOptions<
-  OptionType,
-  GroupBase<OptionType>,
-  unknown
+OptionType,
+GroupBase<OptionType>,
+unknown
 > = async (search, prevOptions) => {
   await sleep(1000);
 
@@ -39,15 +45,15 @@ const loadOptions: LoadOptions<
   } else {
     const searchLower = search.toLowerCase();
 
-    filteredOptions = options.filter(({ label }) =>
-      label.toLowerCase().includes(searchLower)
+    filteredOptions = options.filter(
+      ({ label }) => label.toLowerCase().includes(searchLower),
     );
   }
 
   const hasMore = filteredOptions.length > prevOptions.length + 10;
   const slicedOptions = filteredOptions.slice(
     prevOptions.length,
-    prevOptions.length + 10
+    prevOptions.length + 10,
   );
 
   return {
@@ -57,9 +63,7 @@ const loadOptions: LoadOptions<
 };
 
 export function Simple(props: StoryProps): ReactElement {
-  const [value, onChange] = useState<
-    OptionType | MultiValue<OptionType> | null
-  >(null);
+  const [value, onChange] = useState<OptionType | MultiValue<OptionType> | null>(null);
 
   return (
     <div
@@ -76,70 +80,3 @@ export function Simple(props: StoryProps): ReactElement {
     </div>
   );
 }
-
-const waitForMutation = async (element: HTMLElement, callback: () => void) => {
-  return new Promise<void>((resolve) => {
-    const observer = new MutationObserver((mutationsList) => {
-      for (const mutation of mutationsList) {
-        if (mutation.type === "childList") {
-          callback();
-          observer.disconnect();
-          resolve();
-        }
-      }
-    });
-
-    observer.observe(element, { childList: true, subtree: true });
-  });
-};
-
-export const playSimple = async ({
-  canvasElement,
-  step,
-}: {
-  canvasElement: HTMLElement;
-  step: any;
-}) => {
-  const canvas = within(canvasElement);
-
-  await step("Click on the Select to display the options list", async () => {
-    const select = canvas.getByRole("combobox");
-    await userEvent.click(select, {
-      delay: 400,
-    });
-
-    await waitFor(() => {
-      expect(canvas.getByRole("listbox")).toBeVisible();
-    });
-  });
-
-  await step(
-    "Scroll the options list to the end of first pagination page",
-    async () => {
-      await waitFor(() => {
-        const listbox = canvas.getByRole("listbox");
-        fireEvent.scroll(listbox, { target: { scrollTop: 500 } });
-
-        expect(canvas.getByText("Option 10")).toBeVisible();
-      });
-    }
-  );
-
-  await step(
-    "Scroll the options list to the end of second pagination page",
-    async () => {
-      await waitFor(
-        () => {
-          const listbox = canvas.getByRole("listbox");
-          fireEvent.scroll(listbox, { target: { scrollTop: 500 } });
-
-          expect(canvas.getByText("Option 20")).toBeVisible();
-        },
-        {
-          // interval: 100,
-          timeout: 4000,
-        }
-      );
-    }
-  );
-};
