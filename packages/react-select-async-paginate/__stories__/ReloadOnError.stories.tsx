8d6644e4f970ea144c3f2e92369d5996841385ee
echo-vladimir <echo.vladimir.k@gmail.com>
2024-09-24 16:00:00 +0200

    Implement interaction story for ReloadOnError case

---
+++ packages/react-select-async-paginate/__stories__/ReloadOnError.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { ReloadOnError } from "./ReloadOnError";
+
+const meta: Meta<typeof ReloadOnError> = {
+  title: "react-select-async-paginate/ReloadOnError",
+  component: ReloadOnError,
+};
+export default meta;
+type Story = StoryObj<typeof ReloadOnError>;
+
+export const ReloadOnErrorStory: Story = {
+  name: "Component",
+  render: (props) => <ReloadOnError {...props} />,
+};
